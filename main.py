8537b385afdb52dfd025f76fc045adda99324921
fdceaofcned <114680172+fdceaofcned@users.noreply.github.com>
2022-10-10 20:15:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from tensorflow.python import keras
 from iocluster import *
 model_path = 'disp.h5'
-image_path = 'accept_pure_img/1010-5066.txt.png'
+image_path = '1010-5066.txt.png' #The test file is in the departion.zip compressed file
 model = keras.models.load_model(model_path)
 a1 = predict_task(image_path,model)
 print('the predict number is {}'.format(a1))
